dcc6b6b26f5395e7950f8d61d5b8d0652df958c2
yokouka <ne201221@senshu-u.jp>
2022-11-23 01:50:30 +0900

    mebo api接続できた

---  Practice/mebo.py
+++  Practice/mebo.py
@@ -1,13 +1,14 @@
 from urllib import request
 import requests
-msg_q="こんにちは"
+import json
+msg_q="おやすみ"
 # APIをたたく
 def sendRequest(msg_q):
   url = 'https://api-mebo.dev/api'
 
   item_data = {
-    "api_key":"<e6af416b-f0fd-4f45-9e23-345ed586734c18433f3e385177>",
-    "agent_id":"<0ee8a82d-d64e-4c87-aab8-f40f4ff63de8182b18b7f8714e>",
+    "api_key":"f913984a-5200-4d05-b299-2c6e72f136c2184615a01781c1",
+    "agent_id":"0ee8a82d-d64e-4c87-aab8-f40f4ff63de8182b18b7f8714e",
     "utterance":msg_q,
     "uid":"<YOUR-UID-HERE>"
   }
@@ -18,9 +19,10 @@ def sendRequest(msg_q):
 def getAnswerMsg(msg_q):
   res = sendRequest(msg_q)
   print(res)
-#   res_json = res.json()
+  res_json = res.json()
+  print(res.json())
 
-#   return res_json["bestResponse"]["utterance"]
+  return res_json["bestResponse"]["utterance"]
 if __name__ == '__main__':
     
     getAnswerMsg(msg_q)
